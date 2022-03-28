56e5b861320b0292d26beb3f1ed467aaea71a948
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-03-28 20:57:48 -0300

    server changes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello from server!" });
 });
 
 app.listen(5000, () => {
--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => console.log(data.message));
+  }, []);
+
   return (
     <div className="App">
       <input type="file" id="imageFile" capture="user" accept="image/*" />
