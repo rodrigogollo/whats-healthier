e748e13d646bd5d4f2b2924bef6ea987c84bdd52
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-04-26 20:00:54 -0300

    request dos dados do GTIN via input number funcionando

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+var cors = require('cors')
+const axios = require('axios')
 
-app.get('/api', (req, res) => {
-    res.json({ message: "Hello from server!" });
+app.use(cors());
+
+const config = {
+    headers: {
+        'X-Cosmos-Token': 'D7Xa8_zfeB7TsGMbthhYGg'
+    }
+}
+
+app.get('/api/:gtin', async (req, res) => {
+    console.log('entrou /api/', req.params.gtin)
+    const response = await axios.get(`https://api.cosmos.bluesoft.com.br/gtins/${req.params.gtin}`, config);
+    const data = response.data;
+    console.log(data)
+    res.send(data);
 });
 
 app.listen(5000, () => {
--- src/App.js
+++ src/App.js
@@ -1,19 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Quagga from '@ericblade/quagga2'; // ES6
 
-function App() {
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => console.log(data.message));
-  }, []);
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      file: [],
+      gtin: 7891991000826
+    }
+    const Quagga = require('@ericblade/quagga2').default; 
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.handleNumberChange = this.handleNumberChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  // React.useEffect(() => {
+  //  fetch("https://api.cosmos.bluesoft.com.br/gtins/7891991000826")
+  //  .then((res) => res.json())
+  //  .then((data) => console.log(data.message));
+  // }, []);
 
-  return (
-    <div className="App">
-      <input type="file" id="imageFile" capture="user" accept="image/*" />
-    </div>
-  );
+  handleFileChange(e){
+    this.setState({file: e.target.files[0]})
+
+    Quagga.decodeSingle({
+      decoder: {
+        readers : ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader",
+        "2of5_reader", "code_93_reader", "code_32_reader"]
+      },
+      locate: true, // try to locate the barcode in the image
+      src: e.target.value // or 'data:image/jpg;base64,' + data
+  }, function(result){
+      if(result.codeResult) {
+          console.log("result", result.codeResult.code);
+      } else {
+          console.log("not detected");
+      }
+  });
+  }
+
+  handleNumberChange(e){
+    this.setState({gtin:e.target.value})
+  }
+
+  handleClick(){
+    fetch(`http://localhost:5000/api/${this.state.gtin}`)
+    .then((res) => res.json())
+    .then((data) => console.log('chegou', data))
+    .catch(err => console.log("Errro", err))
+  }
+
+  componentDidMount(){
+    Quagga.init({
+      inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: this.state.file   // Or '#yourElement' (optional)
+      },
+      decoder : {
+        readers : ["code_128_reader"]
+      }
+    }, function(err) {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+    });
+    Quagga.onDetected((data)=> {
+      console.log("detectado")
+      console.log(data);
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <label htmlFor='imageFile'>Selecione um código de barras</label>
+        <br />
+        <input type="file" id="imageFile" capture="user" accept="image/*" onChange={this.handleFileChange} />
+        <br />
+        <label htmlFor="gtin">Digite o Número do Código de Barras:</label>
+        <br />
+        <input type="number" value={this.state.gtin} id="gtin" onChange={this.handleNumberChange} />
+        <button onClick={this.handleClick}>Submit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
