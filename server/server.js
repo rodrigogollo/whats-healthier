56070a6c97ed61f4cd0036bd6c877089b00de904
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-05-05 22:09:26 -0300

    criado a função format para comparar os produtos sem acentos e caractéres especiais

--- server/server.js
+++ server/server.js
@@ -22,18 +22,23 @@ app.get('/api/:gtin', async (req, res) => {
 });
 
 app.get('/api/info/:desc',  async (req, res) => {
-    let foodListDescArray = foodList.map(it => it.description.toUpperCase());
-    console.log(req.params.desc)
-    var stringMatch = stringSimilarity.findBestMatch(req.params.desc.toUpperCase(), foodListDescArray);
-    let info = [];
     
+    let produto = format(req.params.desc)
+    let foodListDescArray = foodList.map(it => format(it.description));
+    console.log(req.params.desc)
+    var stringMatch = stringSimilarity.findBestMatch(produto, foodListDescArray);
+    let info = []
     console.log(stringMatch)
     if(stringMatch.bestMatch.rating >= 0.51) {
-        info = foodList.filter(item => item.description.toUpperCase() === stringMatch.bestMatch.target)
-    }
+        info = foodListDescArray.filter(item => item.description === stringMatch.bestMatch.target)
+    } 
     res.send(info[0]);
 })
 
 app.listen(5000, () => {
     console.log('server running in port ', 5000);
 })
+
+function format(str){
+    return str.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+}
