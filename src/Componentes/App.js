165f16714bba179abd7bf8e913c93f9cfc5b30a1
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-05-16 21:43:18 -0300

    adicionado o escaneador via camera

--- src/Componentes/App.js
+++ src/Componentes/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
-import Quagga from '@ericblade/quagga2'; // ES6
 import Info from './Info';
 import NutritionalInfo from './NutritionalInfo';
 import Recomendados from './Recomendados';
-import {traduzir} from '../Utils/traduzir';
+import { traduzir } from '../Utils/traduzir';
+import Scanner from "./Scanner";
 
 
 class App extends Component {
@@ -13,19 +13,21 @@ class App extends Component {
     this.state = {
       checkedOne: true,
       checkedTwo: false,
-      file: [],
       gtin: 7891991000826,
       comorbidade: 'diabetes',
       data: {
         image: '',
         description: '',
-      }
+      },
+      camera: false,
+      result: null,
     }
-    this.handleFileChange = this.handleFileChange.bind(this);
+
     this.handleNumberChange = this.handleNumberChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleChangeOne = this.handleChangeOne.bind(this);
     this.handleChangeTwo = this.handleChangeTwo.bind(this);
+    this.onDetected = this.onDetected.bind(this);
   }
 
   //REFRIGERANTE GUARANÁ = 7891991000826
@@ -36,25 +38,6 @@ class App extends Component {
   //LEITE CONDENSADO - 7896590817035
   //BISCOITO TRAQUINAS - 7622210592750
   //MANGA ESPADA - 7898205079357
-
-  handleFileChange(e){
-    this.setState({file: e.target.files[0]})
-
-    Quagga.decodeSingle({
-      decoder: {
-        readers : ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader",
-        "2of5_reader", "code_93_reader", "code_32_reader"]
-      },
-      locate: true, // try to locate the barcode in the image
-      src: e.target.value // or 'data:image/jpg;base64,' + data
-  }, function(result){
-      if(result.codeResult) {
-          console.log("result", result.codeResult.code);
-      } else {
-          console.log("not detected");
-      }
-  });
-  }
 
   handleNumberChange(e){
     this.setState({gtin:e.target.value})
@@ -136,33 +119,29 @@ class App extends Component {
     }
   }
 
-  componentDidMount(){
-    Quagga.init({
-        inputStream : {
-          name : "Live",
-          type : "LiveStream",
-          target: this.state.file   // Or '#yourElement' (optional)
-        },
-        decoder : {
-          readers : ["code_128_reader"]
-        }
-      }, function(err) {
-          if (err) {
-              console.log(err);
-              return
-          }
-          console.log("Initialization finished. Ready to start");
-          Quagga.start();
-      });
-      Quagga.onDetected((data)=> {
-        console.log("detectado")
-        console.log(data);
-      })
+  onDetected(result) {
+    this.setState({result: result})
   }
 
   render(){
+ 
     return (
       <div className="App">
+         <div className="Scanner">
+          <p>{this.state.result ? this.state.result : "Escaneando..."}</p>
+          <button onClick={() => this.setState(prevState => {
+            return {
+              ...prevState,
+              camera: !prevState.camera
+            }
+          })}>
+            {this.state.camera ? "Stop" : "Start"}
+          </button>
+          <div className="container">
+            {this.state.camera && <Scanner onDetected={this.onDetected} />}
+          </div>
+        </div>
+
         <div className="comorbidades">
           <h2>Comorbidades</h2>
           <h3>Selecione sua Comorbidade:</h3>
@@ -177,18 +156,15 @@ class App extends Component {
             onChange={this.handleChangeTwo}
           />
         </div>
+
         <div className="codigoBarras">
-          <div className="Camera">
-            <label htmlFor='imageFile'>Selecione um código de barras:
-              <input type="file" id="imageFile" capture="user" accept="image/*" onChange={this.handleFileChange} />
-            </label>
-          </div>
           <div className="Digitar">
             <label htmlFor="gtin">Digite o Número do Código de Barras: 
               <input type="text" value={this.state.gtin} id="gtin" onChange={this.handleNumberChange} />
             </label>
           </div>
         </div>
+
         <button onClick={this.handleClick}>Buscar</button>
         {
           this.state.data ? <Info description={this.state.data.description} image={this.state.data.image} /> : <p></p>
