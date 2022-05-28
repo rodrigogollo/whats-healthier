7cd8db2933fe90c44d2173aea5ca08a39fb45e26
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-05-28 15:25:46 -0300

    inclusão da categoria no titulo de produtos recomendados

--- src/Componentes/Recomendados.js
+++ src/Componentes/Recomendados.js
@@ -1,14 +1,17 @@
 import NutritionalInfo from './NutritionalInfo';
 import './Recomendados.css';
+import categoryList from '../TACO/categoryList.json';
 
-const Recomendados = ({recomendados}) => (
-    <div className='recomendados'>
-        <h1>Produtos Recomendados</h1>
-        <div className='itens'>
-            {
-                recomendados.map((item, i) => <NutritionalInfo key={i} nutritionalInfo={item} /> )
-            }
+const Recomendados = ({recomendados}) => {
+    var categoria = categoryList.filter(item => item.id === recomendados[0].category_id).map(item => item.category);
+    console.log(categoria)
+    return <div className='recomendados'>
+            <h1>{`Produtos Recomendados - ${categoria}`}</h1>
+            <div className='itens'>
+                {
+                    recomendados.map((item, i) => <NutritionalInfo key={i} nutritionalInfo={item} /> )
+                }
+            </div>
         </div>
-    </div>
-)
+}
 export default Recomendados;
